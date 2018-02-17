d1f09e2ab57b8b266a9b35c53eeaf1d346f2d45b
Amit <amitkundani@gmail.com>
2018-02-17 20:06:42 +0000

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,25 @@
+class Post < ActiveRecord::Base
+    validates :title, :body, presence: true
+    
+    def self.titles
+        Post.pluck( :title)
+    end 
+    
+    def word_count
+        body.split(" ").count
+    end 
+    
+    
+    def pretty_word_count
+        "This post has #{word_count} words."
+    end
+    
+    def character_count
+        body.split("").count
+    end 
+    
+    def pretty_character_count
+        "This post has #{character_count} characters."
+        
+    end 
+end
