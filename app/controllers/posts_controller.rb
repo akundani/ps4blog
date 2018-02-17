e2e81cfb3133c7952d8a3f76fa63a53e80158333
Amit Kundani <akundani@gmail.com>
2018-02-17 14:34:07 -0800

    Updates posts endpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,52 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show, :index]
+
   def edit
-    @post= Post.find(params[:id])
+    @post= current_user.posts.find_by(id: params[:id])
+    if @post.nil?
+      flash[:alert] = "Unauthorized"
+      redirect_to root_path
+    end
   end
 
   def update
-  end 
-  
+    @post = current_user.posts.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post Updated"
+      redirect_to post_path(@post)
+    else
+      flash.now[:alert] = @post.errors.full_messages.to_sentence
+      render "new"
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def index
     @posts = Post.all
-  end 
-  
-  def new 
-    @post = Post.new
   end
-  def create 
-  @post = Post.new(post_params)
-  
-  if @post.save
-    redirect_to posts_path
-  else 
-    render "new"
+
+  def new
+    @post = current_user.posts.new
+  end
+
+  def create
+  @post = current_user.posts.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Post Created"
+      redirect_to post_path(@post)
+    else
+      flash.now[:alert] = @post.errors.full_messages.to_sentence
+      render "new"
+    end
+  end
+
+  private
+
+  def post_params
+      params.require( :post).permit( :title, :body)
   end
 end
-
-def post_params
-    params.require( :post).permit( :title, :body)
-end 
-
-end
-
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,25 +1,30 @@
 class Post < ActiveRecord::Base
-    validates :title, :body, presence: true
-    
+    belongs_to :user
+    validates :title, :body, :user, presence: true
+
     def self.titles
         Post.pluck( :title)
-    end 
-    
+    end
+
     def word_count
         body.split(" ").count
-    end 
-    
-    
+    end
+
+    def creator_email
+        user.email
+
+    end
+
     def pretty_word_count
         "This post has #{word_count} words."
     end
-    
+
     def character_count
         body.split("").count
-    end 
-    
+    end
+
     def pretty_character_count
         "This post has #{character_count} characters."
-        
-    end 
+
+    end
 end
